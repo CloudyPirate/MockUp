8b649aa1736900d7b6dd83deb4fb3e2d6d6e6347
Juan Bohorquez <you@example.com>
2025-07-09 16:25:58 -0500

    message

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,7 +3,6 @@ package router
 import (
 	"net/http"
 
-	"github.com/PirateWindows/DriverMockup/internal/handler"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,11 +11,30 @@ func SetupRouter() *gin.Engine {
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/static", "./static")
 	r.GET("/", showMain)
-	r.GET("/users", handler.GetUsers)
-	r.POST("/users", handler.CreateUser)
+	r.GET("/esports", showEsports)
+	r.GET("/lacrosse", showLacrosse)
+	r.GET("/programming", showProgramming)
+	r.GET("/test", showTest)
+	//r.GET("/users", handler.GetUsers)
+	//r.POST("/users", handler.CreateUser)
 	return r
 }
 
 func showMain(c *gin.Context) {
-	c.HTML(http.StatusOK, "main.html", nil)
+	c.HTML(http.StatusOK, "main1.html", nil)
+}
+
+func showEsports(c *gin.Context) {
+	c.HTML(http.StatusOK, "esports.html", nil)
+}
+
+func showLacrosse(c *gin.Context) {
+	c.HTML(http.StatusOK, "lacrosse.html", nil)
+}
+
+func showProgramming(c *gin.Context) {
+	c.HTML(http.StatusOK, "programming.html", nil)
+}
+func showTest(c *gin.Context) {
+	c.HTML(http.StatusOK, "test.html", nil)
 }
